2f93e398533f4b218914bbeae07a223f1ea1aeeb
Andrey4957 <aafolomeef@gmail.com>
2025-08-14 14:34:24 +0300

    fix: use existing VPC network

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,18 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.126"
-    }
-  }
-}
-
 provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
-  service_account_key_file = var.service_account_key_file
+  zone      = "ru-central1-a"
 }
 
-resource "yandex_vpc_network" "net" {
-  name = "net-auto"
+# Используем существующую сеть
+data "yandex_vpc_network" "default" {
+  name = "default" # или поменяй на имя своей сети
 }
 
 resource "yandex_vpc_subnet" "subnet" {
   name           = "subnet-auto"
-  zone           = var.zone
-  network_id     = yandex_vpc_network.net.id
+  zone           = "ru-central1-a"
+  network_id     = data.yandex_vpc_network.default.id
   v4_cidr_blocks = ["10.10.0.0/24"]
 }
 
@@ -33,18 +23,21 @@ data "yandex_compute_image" "ubuntu" {
 resource "yandex_compute_instance" "vm" {
   name        = "tf-vm-a"
   platform_id = "standard-v1"
-  zone        = var.zone
+  zone        = "ru-central1-a"
 
   resources {
-    cores  = 2
-    memory = 2
+    cores         = 2
+    memory        = 2
+    core_fraction = 100
   }
 
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.id
       size     = 10
+      type     = "network-hdd"
     }
+    auto_delete = true
   }
 
   network_interface {
@@ -53,6 +46,10 @@ resource "yandex_compute_instance" "vm" {
   }
 
   metadata = {
-    ssh-keys = "${var.ssh_user}:${var.ssh_public_key}"
+    ssh-keys = "ваш_пользователь_на_вм:ssh-rsa AAAA..." # не забудь заменить
   }
+}
+
+output "public_ip" {
+  value = yandex_compute_instance.vm.network_interface.0.nat_ip_address
 }
