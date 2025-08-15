826bd57f4b82ef53659024850a69d6bc837093eb
Andrey4957 <aafolomeef@gmail.com>
2025-08-15 11:15:51 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-cat > terraform/main.tf <<'HCL'
+# Используем существующую сеть "default"
 data "yandex_vpc_network" "default" {
   name = "default"
 }
@@ -10,6 +10,17 @@ resource "yandex_vpc_subnet" "subnet" {
   v4_cidr_blocks = ["10.10.0.0/24"]
 }
 
+# Разрешим SSH и HTTP (чтобы Ansible и nginx не упирались в фаервол)
+resource "yandex_vpc_security_group" "web_sg" {
+  name       = "web-sg"
+  network_id = data.yandex_vpc_network.default.id
+
+  ingress { protocol = "TCP" port = 22 v4_cidr_blocks = ["0.0.0.0/0"] }
+  ingress { protocol = "TCP" port = 80 v4_cidr_blocks = ["0.0.0.0/0"] }
+  egress  { protocol = "ANY" from_port = 0 to_port = 65535 v4_cidr_blocks = ["0.0.0.0/0"] }
+}
+
+# Образ Ubuntu LTS
 data "yandex_compute_image" "ubuntu" {
   family = "ubuntu-2004-lts"
 }
@@ -37,10 +48,10 @@ resource "yandex_compute_instance" "vm" {
   network_interface {
     subnet_id = yandex_vpc_subnet.subnet.id
     nat       = true
+    security_group_ids = [yandex_vpc_security_group.web_sg.id]
   }
 
   metadata = {
     "ssh-keys" = "${var.ssh_user}:${var.ssh_public_key}"
   }
 }
-HCL
